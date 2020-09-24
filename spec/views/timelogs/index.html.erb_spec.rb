555163fd92c0fb9d16f2dcce72185c61264f4784
Roeland Matthijssens <roeland.matthijssens@gmail.com>
2020-09-24 14:50:57 +0200

    Render names instead of IDs in timelog table

--- spec/views/timelogs/index.html.erb_spec.rb
+++ spec/views/timelogs/index.html.erb_spec.rb
@@ -4,14 +4,31 @@ require 'rails_helper'
 
 RSpec.describe 'timelogs/index', type: :view do
   before(:each) do
-    assign(:timelogs, [
-             create(:timelog),
-             create(:timelog)
-           ])
+    @timelogs = assign(:timelogs, [
+                         create(:timelog),
+                         create(:timelog)
+                       ])
   end
 
   it 'renders a list of timelogs' do
     render
     assert_select 'tbody>tr', 2
+  end
+
+  it 'should show the project name in the table' do
+    render
+    @timelogs.each do |timelog|
+      expect(rendered).to match(/#{timelog.project.name}/)
+    end
+  end
+  it 'should show the user email in the table' do
+    render
+    @timelogs.each do |timelog|
+      expect(rendered).to match(/#{timelog.user.email}/)
+    end
+  end
+  it 'should have a `billable` column in the table' do
+    render
+    expect(rendered).to match(/Billable/)
   end
 end
