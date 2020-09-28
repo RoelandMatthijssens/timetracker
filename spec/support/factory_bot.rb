d130566650340a4a6964954a88832b8267efddb3
Roeland Matthijssens <roeland.matthijssens@gmail.com>
2020-09-28 16:35:06 +0200

    Add authentication specs for timelogs

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,3 +3,21 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+module FactoryBot
+  module Syntax
+    module Methods
+      def nested_attributes_for(*args)
+        attributes = attributes_for(*args)
+        klass = args.first.to_s.camelize.constantize
+
+        klass.reflect_on_all_associations(:belongs_to).each do |r|
+          association = FactoryBot.create(r.class_name.underscore)
+          attributes["#{r.name}_id"] = association.id
+          attributes["#{r.name}_type"] = association.class.name if r.options[:polymorphic]
+        end
+
+        attributes
+      end
+    end
+  end
+end
